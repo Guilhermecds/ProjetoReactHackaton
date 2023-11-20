a986191cb8d7112953c562483c5ed586183d3c69
Lucas Bonfim <Lucasricobonfim>
2023-11-20 20:40:13 -0300

    feat: iniciando projeto react do hackton

---
+++ reacthackaton/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Header } from './Components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <Header />
+
+        <h1>TESTE</h1>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ reacthackaton/src/Components/Header/index.tsx
@@ -0,0 +1,17 @@
+import  "./estilo.css"
+
+interface PropsHeader {
+    // abrirModal: () => void;
+}
+
+
+export const Header = (props: PropsHeader) => {
+    return(
+     
+        <header>
+            <h1>Transporte publico</h1>
+        </header>
+    
+     
+    )
+}
