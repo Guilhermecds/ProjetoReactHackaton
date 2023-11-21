718501445a0e31ecfb9c713b4fe232fdd387d8bc
Lucas Bonfim <Lucasricobonfim>
2023-11-20 22:04:27 -0300

    feat: inicio formulario de rotas de destinos

--- reacthackaton/src/App.tsx
+++ reacthackaton/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Header } from './Components/Header'
+import { Rotas } from './Components/Rotas'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,9 @@ function App() {
 
       <Header />
 
-        <h1>TESTE</h1>
+      <Rotas />
+{/* 
+        <h1>TESTE</h1> */}
 
 
     </>
---
+++ reacthackaton/src/Components/Rotas/styles.ts
@@ -0,0 +1,55 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+
+// Criando os componentes estilizados
+export const FormContainer = styled.form`
+  max-width: 400px;
+  margin: 20px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 20px;
+  text-align:center;
+`;
+
+export const Label = styled.label`
+  display: block;
+  font-size: 14px;
+  margin-bottom: 5px;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-left: -7px;
+  font-size: 14px;
+  border: 2px solid ;
+  border-radius: 10px;
+`;
+
+export const Button = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  margin: 10px auto;
+  padding: 15px 100px;
+  font-size: 16px;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+export const Icon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 242px;
+  color: black;
+`;
